5f911578c28e3d19beb4d746a3b3489ca0a1a190
YUU522 <kuromu2750@gmail.com>
2023-08-15 08:26:16 +0000

    [about Update]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def new
+    @book=Book.new
+  end
+
+ # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
+  def index
+    @books = Book.all
+  end
+  
+  def destroy
+    @book = Book.find(book_params)
+    book.destroy
+    redirect_to books_path(book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :image, :body)
+  end
+end
